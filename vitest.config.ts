b18dec8e192f6ecbf05a7e79ad036454359e95bc
businesscleanfreaks <business.cleanfreaks@gmail.com>
2026-07-05 11:04:16 +0500

    Run unit tests in Node environment

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
+    environment: 'node',
     setupFiles: ['./vitest.setup.ts'],
     include: ['__tests__/lib/**/*.test.{ts,tsx}'],
     globals: true,
